e4dff4a465a25873243031dd27de3f15ce8b90ae
Kevin Rojas <kevinrojas99@gmail.com>
2026-03-27 12:41:37 -0400

    Modifying vae training and adding uv files

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from drive-download-20260313t165412z-3-001!")
+
+
+if __name__ == "__main__":
+    main()
--- train_vae.py
+++ train_vae.py
@@ -4,67 +4,56 @@ import PIL
 import click
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
 from diffusers import AutoencoderKL
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-class CUB_200_2011(torch.utils.data.Dataset):
-    def __init__(self, path, split='all', transform=None, **kwargs):
-        super().__init__()
+from Data_loader_PKL import Chr20MultimodalDataset
 
-        assert split in ('all', 'train_val', 'test')
 
-        self.split = split
-        self.transform = transform
+class _HicOnly(torch.utils.data.Dataset):
+    """Wrap multimodal dataset; VAE trains on Hi-C maps (1, 100, 100) only."""
 
-        CUB_200_2011_PATH = path
-        metadata = torch.load(os.path.join(CUB_200_2011_PATH, 'metadata.pth'), weights_only=True)
-
-        # labels
-        self.img_id_to_class_id = metadata['img_id_to_class_id']
-        self.class_id_to_class_name = metadata['class_id_to_class_name']
-        self.class_name_to_class_id = metadata['class_name_to_class_id']
-
-        # captions
-        self.img_id_to_encoded_caps = metadata['img_id_to_encoded_caps']
-        self.word_id_to_word = metadata['word_id_to_word']
-        self.word_to_word_id = metadata['word_to_word_id']
-
-        name = self.split +'_' if split != 'all' else ''
-        imgs_path = os.path.join(CUB_200_2011_PATH, f'imgs_{name}128x128.pth')
-        self.img_ids = metadata[f'{name}img_ids']
-        self.imgs = torch.load(imgs_path)
-
-    def decode_caption(self, cap):
-        if isinstance(cap, torch.Tensor):
-            cap = cap.tolist()
-
-        arr = [self.word_id_to_word[word_id] for word_id in cap]
-        arr = [a for a in arr if a != '<UNKNOWN>']
-        return ' '.join(arr)
+    def __init__(self, base: Chr20MultimodalDataset):
+        self.base = base
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.base)
 
     def __getitem__(self, idx):
-        img = self.imgs[idx]
-        if self.transform:
-            img = self.transform(img)
-        img_id = self.img_ids[idx]
-        encoded_caps = self.img_id_to_encoded_caps[img_id]
-        captions = [ self.decode_caption(cap) for cap in encoded_caps]
-        return img, captions
+        hic, *_ = self.base[idx]
+        return hic, 0
+
+
+def get_dataset(cache_dir: str, enformer_tsv: str):
+    full = Chr20MultimodalDataset(cache_dir, enformer_tsv)
+    wrapped = _HicOnly(full)
+    n = len(wrapped)
+    train_size = int(0.7 * n)
+    val_size = int(0.15 * n)
+    test_size = n - train_size - val_size
+    generator = torch.Generator().manual_seed(42)
+    train_dataset, _val_dataset, test_dataset = random_split(
+        wrapped, [train_size, val_size, test_size], generator=generator
+    )
+    return train_dataset, test_dataset, 1
+
+
+def _imshow_chw(ax, t):
+    t = t.detach().float().cpu().clamp(0, 1)
+    if t.shape[0] == 1:
+        ax.imshow(t[0].numpy(), cmap='gray', vmin=0, vmax=1)
+    else:
+        ax.imshow(t.permute(1, 2, 0).numpy())
 
 
 def plot_samples(images, encoded, reconstructed_images):
     for j in range(4):
-        fig, ax = plt.subplots(1,3)
-        ax[0].imshow(encoded[j].clamp(0,1).permute(1,2,0).cpu().detach().numpy())
-        ax[1].imshow(images[j].clamp(0,1).permute(1,2,0).cpu().detach().numpy())
-        ax[2].imshow(reconstructed_images[j].clamp(0,1).permute(1,2,0).cpu().detach().numpy())
-                        
+        fig, ax = plt.subplots(1, 3)
+        ax[0].imshow(encoded[j].clamp(0, 1).permute(1, 2, 0).cpu().detach().numpy())
+        _imshow_chw(ax[1], images[j])
+        _imshow_chw(ax[2], reconstructed_images[j])
         fig.savefig(f'sample_{j}.png')
         plt.close(fig)
         
@@ -82,41 +71,22 @@ def loss_fn(autoencoder, images, al, return_images=False):
     else:
         return loss
 
-def get_dataset(name, res):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(res)])
-    if name == 'cifar10':
-        train_dataset = datasets.CIFAR10(root='./saved_datasets', train=True, download=True, transform=transform)
-        test_dataset = datasets.CIFAR10(root='./saved_datasets', train=False, download=True, transform=transform)
-        return train_dataset, test_dataset, 3
-    if name == 'cifar100':
-        train_dataset = datasets.CIFAR100(root='./saved_datasets', train=True, download=True, transform=transform)
-        test_dataset = datasets.CIFAR100(root='./saved_datasets', train=False, download=True, transform=transform)
-        return train_dataset, test_dataset, 3
-    elif name == 'mnist':
-        train_dataset = datasets.MNIST(root='./saved_datasets', train=True, download=True, transform=transform)
-        test_dataset = datasets.MNIST(root='./saved_datasets', train=False, download=True, transform=transform)
-        return train_dataset, test_dataset, 1
-    elif name == 'cub-200':
-        transform = transforms.Lambda(lambda x: (x.float() / 255.) * 2. - 1.)
-        train_dataset = CUB_200_2011('/network/rit/dgx/dgx_Yelab/kevin_rojas/datasets/cob-200','train_val', transform=transform)
-        test_dataset = CUB_200_2011('/network/rit/dgx/dgx_Yelab/kevin_rojas/datasets/cob-200','all', transform=transform)
-        return train_dataset, test_dataset, 3
-
 @click.command()
-@click.option('--dataset', type=click.Choice(['cifar10','cifar100','mnist', 'cub-200']), default='cub-200')
-@click.option('--starting_res', type=int, default=32)
+@click.option('--cache_dir', type=str, default='./Chr20_all_cached')
+@click.option('--enformer_tsv', type=str, default='./Chr20_all_cached/enfemb_chr20_res1000_dim512.tsv.gz')
+@click.option('--starting_res', type=int, default=100, help='Hi-C maps are 100x100; used for block_out_channels.')
 @click.option('--num_blocks', type=int, default=2)
 @click.option('--latent_channels', type=int, default=4)
 @click.option('--batch_size', type=int, default=256)
 @click.option('--num_epochs', type=int, default=50)
 @click.option('--al', type=float, default=.99999)
 @click.option('--dir', type=str, default=None)
-def train(dataset, starting_res, latent_channels, num_blocks, batch_size, num_epochs, al, dir):
+def train(cache_dir, enformer_tsv, starting_res, latent_channels, num_blocks, batch_size, num_epochs, al, dir):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     if dir is None:
-        dir = f'./{dataset}_vae'
+        dir = './chr20_vae'
         
-    train_dataset, test_dataset, in_channels = get_dataset(dataset, starting_res) 
+    train_dataset, test_dataset, in_channels = get_dataset(cache_dir, enformer_tsv)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
@@ -178,13 +148,19 @@ def train(dataset, starting_res, latent_channels, num_blocks, batch_size, num_ep
         encoded = latent_dist.sample()
         reconstructed_images = autoencoder.decode(encoded).sample
 
-        folder = os.path.join(dir, f'reconstructed_{dataset}')
+        folder = os.path.join(dir, 'reconstructed_chr20')
         os.makedirs(folder, exist_ok=True)
-        images_np = (reconstructed_images * 255 ).clip(0, 255).to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy()
-
-        for i in range(len(images_np)):
-            PIL.Image.fromarray(images_np[i], 'RGB').save(os.path.join(folder, f'{k}.png'))
-            k+=1
+        recon = reconstructed_images.clamp(0, 1)
+        if recon.shape[1] == 1:
+            for i in range(recon.shape[0]):
+                arr = (recon[i, 0] * 255).byte().cpu().numpy()
+                PIL.Image.fromarray(arr, mode='L').save(os.path.join(folder, f'{k}.png'))
+                k += 1
+        else:
+            images_np = (recon * 255).to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy()
+            for i in range(len(images_np)):
+                PIL.Image.fromarray(images_np[i], 'RGB').save(os.path.join(folder, f'{k}.png'))
+                k += 1
 
 if __name__ == '__main__':
     train()
